2af3d4613e1fe5adc4af62ee1901c194869648b4
Michelle <rotariumihaela.bv@gmail.com>
2023-09-06 21:39:16 +0300

    Add project files.

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSignalR.Server.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await SendMessage("", "User connected!");
+            await base.OnConnectedAsync();
+        }
+
+        public async Task SendMessage(string user,string message)
+        {
+            await Clients.All.SendAsync("GetThatMessageDude",user,message);
+        }
+    }
+}
